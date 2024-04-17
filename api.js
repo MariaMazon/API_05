4d9ad130d38d4acb3e69f083ea9ed65b3eb3c3e6
duda <dudalaumazon@gmail.com>
2024-04-17 15:31:51 -0300

    subir arquivos iniciais

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-// require 
+// 17/04 criar dois novos endpoints
 
 const express = require('express');
 const mysql = require('mysql2');
@@ -6,18 +6,19 @@ const cors = require('cors');
 
 const mysql_config = require('./imp/mysql_config');
 const functions = require('./imp/functions');
+const { json } = require('express/lib/response');
 
-//variáveis para disponibilidade e para versionamento
+
 const  API_AVAILABILITY = true;
 const API_VERSION = '1.0.0';
 
-//iniciar servidor 
+
 const app = express();
 app.listen(3000,()=>{
     console.log("API está executando!");
 })
 
-// verificar a disponibilidade da API
+
 app.use((req,res,next)=>{
     if(API_AVAILABILITY){
         next()
@@ -26,15 +27,13 @@ app.use((req,res,next)=>{
     }
 })
 
-// conexão com mysql
 
 const connection = mysql.createConnection(mysql_config);
 
-//cors
+
 app.use(cors());
 
-//rotas 
-//rota inicial
+
 app.get('/',(req,res)=>{
     res.json(functions.response("sucesso","API está rodando!",0,null));
 })
@@ -51,8 +50,41 @@ app.get('/tasks',(req,res)=>{
     })
 })
 
-//tratar erro da rota
+//rota para fazer uma consulta de tarefa por id
+app.get('/tasks/:id',(req,res)=>{
+    const id = req.params.id;
+    connection.query('SELECT* FROM  tasks  WHERE id = ?',[id],(err,rows)=>{
+        if(!err){
+            if(rows.length>0){
+                res.json(functions.response('Sucesso','Sucesso na pesquisa',rows.length,rows))
+            }else{
+                res.json(functions.response('Atenção!','Não foi encontrada a task selecionada',0,null))
+            }
+        }else{
+            res.json(functions.response('erro',err.message,0,null))
+        }
+    })
+})
+
+//rota para atualizar o status da task pelo id selecionado
+app.put('/tasks/:id/status/:status',(req,res)=>{
+    const id = req.params.id;
+    const status = req.params.status;
+    connection.query('UPDATE tasks SET status = ? WHERE id = ?',[status,id],(err,rows)=>{
+        if(!err){
+            if(rows.affectedRows>0){
+                res.json(functions.response('Sucesso','Sucesso na alteração do status',rows.affectedRows,null))
+            }else{
+                res.json(functions.response('Alerta vermelh','Task não encontrado!',0,null))
+            }
+        }else{
+            res.json(functions.response('Erro',err.message,0,null))
+        }
+    })
+})
 
 app.use((req,res)=>{
     res.json(functions.response('Atenção! Rota não encontrada',0,null))
 })
+
+
